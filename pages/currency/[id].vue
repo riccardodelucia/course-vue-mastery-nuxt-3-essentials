64c7a7d46789a7988dd9ae723c5387b17e9076c2
Riccardo Roberto De Lucia <riccardoroberto.delucia@gmail.com>
2024-01-28 11:37:38 +0100

    Initial Commit

---
+++ pages/currency/[id].vue
@@ -0,0 +1,29 @@
+<script setup>
+  const route = useRoute();
+  const { data } = await useFetch(`/api/ticker/?id=${route.params.id}`);
+  const coin = data.value[0];
+</script>
+
+<template>
+  <div>
+    <h2>{{ coin.name }} Detail Page</h2>
+    <table border="1px solid">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Symbol</th>
+          <th>Price - US $</th>
+          <th>Market Cap - US $</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{{ coin.name }}</td>
+          <td>{{ coin.rank }}</td>
+          <td>{{ coin.price_usd }}</td>
+          <td>{{ coin.market_cap_usd }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
